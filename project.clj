6482c88415822be70841dce4c6d33ee8486e5ea8
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2016-12-19 20:31:52 +0000

    jar-hell sticking plaster

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject employeerepublic/slf4j-timbre "0.4.1"
+(defproject employeerepublic/slf4j-timbre "0.4.2"
   :description "SLF4J binding for Timbre"
   :url "https://github.com/employeerepublic/slf4j-timbre"
   :scm {:name "git"
@@ -7,7 +7,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.taoensso/timbre "4.7.4"]
+                 [org.clojure/tools.reader "1.0.0-beta3"]
+                 [com.taoensso/timbre "4.7.4"
+                  :exclusions [org.clojure/tools.reader]]
                  [org.slf4j/slf4j-api "1.7.21"]]
   :profiles {:dev
              {:dependencies [[midje "1.8.3"]]
