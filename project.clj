40e80722dee4bdd33dcba5a2d756b8eff594d662
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2017-05-11 22:57:27 +0100

    update deps

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject employeerepublic/slf4j-timbre "0.4.2"
+(defproject employeerepublic/slf4j-timbre "0.4.3"
   :description "SLF4J binding for Timbre"
   :url "https://github.com/employeerepublic/slf4j-timbre"
   :scm {:name "git"
@@ -6,12 +6,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.reader "1.0.0-beta3"]
-                 [com.taoensso/timbre "4.7.4"
-                  :exclusions [org.clojure/tools.reader]]
-                 [org.slf4j/slf4j-api "1.7.21"]]
-  :profiles {:dev
+  :profiles {:provided
+             {:dependencies
+              [[org.clojure/clojure "1.8.0"]
+               [org.clojure/tools.reader "1.0.0-beta3"]
+               [com.taoensso/timbre "4.10.0"
+                :exclusions [org.clojure/tools.reader]]
+               [org.slf4j/slf4j-api "1.7.25"]]}
+
+             :dev
              {:dependencies [[midje "1.8.3"]]
               :plugins [[lein-midje "3.2.1"]]}}
   :aot :all)
