767804a51047373291f25d57bc0f64f9912786ea
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2018-11-15 22:11:48 +0000

    deps update

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject employeerepublic/slf4j-timbre "0.4.4"
+(defproject employeerepublic/slf4j-timbre "0.5.0"
   :description "SLF4J binding for Timbre"
   :url "https://github.com/employeerepublic/slf4j-timbre"
   :scm {:name "git"
@@ -8,13 +8,13 @@
 
   :profiles {:provided
              {:dependencies
-              [[org.clojure/clojure "1.8.0"]
-               [org.clojure/tools.reader "1.1.0"]
+              [[org.clojure/clojure "1.9.0"]
+               [org.clojure/tools.reader "1.3.2"]
                [com.taoensso/timbre "4.10.0"
                 :exclusions [org.clojure/tools.reader]]
                [org.slf4j/slf4j-api "1.7.25"]]}
 
              :dev
-             {:dependencies [[midje "1.8.3"]]
+             {:dependencies [[midje "1.9.4"]]
               :plugins [[lein-midje "3.2.1"]]}}
   :aot :all)
